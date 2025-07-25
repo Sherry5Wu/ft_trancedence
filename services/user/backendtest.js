6088749d1392ee18baf5fe00173a04444bec5ef7
Oskari Martela <omartela@c2r5p8.hive.fi>
2025-07-25 14:19:10 +0300

    changes port

--- services/user/backendtest.js
+++ services/user/backendtest.js
@@ -155,7 +155,7 @@ fastify.post('/match_history', (request, reply) => {
 
 
 // Käynnistetään palvelin portissa 3000 (tai .env:n PORT)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 fastify.listen({ port, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
