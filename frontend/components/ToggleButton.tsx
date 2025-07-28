f5c1dea04a4e8c35bea6936c76c59b3ded6d556a
Joel Lehtonen <jlehtone@c1r6p8.hive.fi>
2025-07-28 15:51:50 +0300

    redo

--- frontend/components/ToggleButton.tsx
+++ frontend/components/ToggleButton.tsx
@@ -11,7 +11,7 @@ export const ToggleButton = ( { label }: ToggleButtonProps) => {
         <div>
         <button onClick={() => setIsOn(!isOn)} className={`relative inline-block w-13 h-7 rounded-full transition-all ease-in-out  ${isOn ? 'bg-slate-800' : 'bg-slate-100'}`}>
             <div className={`w-5 h-5 ml-1 rounded-full border border-slate-300 shadow-sm transition-all ease-in-out 
-                            ${isOn ? 'bg-white translate-x-6' : 'bg-slate-800'}`}>
+                            ${isOn ? 'bg-white translate-x-5' : 'bg-slate-800'}`}>
                 {/* {isOn ? 'ON' : 'OFF'} */}
             </div>
         </button>
