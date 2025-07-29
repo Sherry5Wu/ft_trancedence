4292dc41cb9f8bd658550fafb79afc09f2dd6475
Joel Lehtonen <jlehtone@c1r6p8.hive.fi>
2025-07-29 09:17:45 +0300

    feat: toggle button now takes in custom onClick behavior

--- frontend/components/ToggleButton.tsx
+++ frontend/components/ToggleButton.tsx
@@ -2,14 +2,21 @@ import { useState } from 'react';
 
 interface ToggleButtonProps {
     label?: string;
+    onClick?: () => void;
 }
 
-export const ToggleButton = ( { label }: ToggleButtonProps) => {
+export const ToggleButton = ( { label, onClick }: ToggleButtonProps) => {
     const [isOn, setIsOn] = useState(false);
+
+    const handleClick = () => {
+        if (onClick)
+            onClick();
+        setIsOn(!isOn);
+    }
 
     return (
         <div>
-        <button onClick={() => setIsOn(!isOn)} className={`relative inline-block w-13 h-7 rounded-full transition-all ease-in-out  ${isOn ? 'bg-slate-800' : 'bg-slate-100'}`}>
+        <button onClick={handleClick} className={`relative inline-block w-13 h-7 rounded-full transition-all ease-in-out  ${isOn ? 'bg-slate-800' : 'bg-slate-100'}`}>
             <div className={`w-5 h-5 ml-1 rounded-full border transition-all ease-in-out 
                             ${isOn ? 'bg-white translate-x-6' : 'bg-slate-800'}`}>
                 {/* {isOn ? 'ON' : 'OFF'} */}
