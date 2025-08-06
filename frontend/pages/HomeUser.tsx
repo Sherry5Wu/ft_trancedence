2013e3aa667004e6438898e650d4319f0d9fd7fa
Joel Lehtonen <jlehtone@c1r6p8.hive.fi>
2025-08-06 17:19:12 +0300

    feat: stats and match history is now hidden behind a button

--- frontend/pages/HomeUser.tsx
+++ frontend/pages/HomeUser.tsx
@@ -1,6 +1,6 @@
 // pages/HomeUser.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GenericButton } from '../components/GenericButton';
 import { MatchHistory } from '../components/MatchHistory';
@@ -19,6 +19,8 @@ import DownArrow from '../assets/noun-down-arrow-down-1144832.svg?react';
 const HomeUserPage = () => {
   const navigate = useNavigate(); // to access other pages
   const { user, setUser } = useUserContext();
+  const [hidden, setHidden] = useState(false);
+
 
   //mockdata
   const playedGames = 77;
@@ -27,8 +29,13 @@ const HomeUserPage = () => {
   const worstRival = 'Alice';
   const WorstRivalPic = <img src='../assets/profilepics/B2.png' className='profilePic mt-1'/>
 
+
+  const showStats = () => {
+      setHidden(!hidden);
+    }
+
     return (
-      <div className='pageLayout'>
+    <div className='pageLayout'>
       
       {/* Username header */}
 
@@ -39,12 +46,12 @@ const HomeUserPage = () => {
       <div className='w-56 truncate mb-12'>
         <h2 className='h2 text-center mb-3 font-semibold scale-dynamic'>{user?.username} </h2>
         <div className='flex justify-between'>
-          <h4 className='h4'>Score</h4>
-          <h4 className='h4 text-right font-semibold'>{user?.score}</h4>
+          <h4 className='h4 ml-2 scale-dynamic'>Score</h4>
+          <h4 className='h4 mr-2 scale-dynamic text-right font-semibold'>{user?.score}</h4>
         </div>
         <div className='flex justify-between'>
-          <h4 className='h4'>Rank</h4>
-          <h4 className='h4 text-right font-semibold'>#{user?.rank}</h4>
+          <h4 className='h4 ml-2 scale-dynamic'>Rank</h4>
+          <h4 className='h4 mr-2 scale-dynamic ext-right font-semibold'>#{user?.rank}</h4>
         </div>
       </div>
 
@@ -84,77 +91,81 @@ const HomeUserPage = () => {
         />
       </div>
 
-      {/* <DownArrow className='size-15' /> */}
+      <button onClick={showStats}> <DownArrow className='size-20 -mb-15' /> </button>
       {/* Statistics */}
       
-      <div className='w-200 scale-90'>
-        <h3 className='h3 text-center font-semibold my-5'>STATS</h3>
-        <div className='grid grid-cols-2 w-full h-200'>
-          <div>
-            <h4 className='h4 text-center my-5 font-semibold'>SCORE HISTORY</h4>
-            <LineGraph />
-          </div>
-          <div>
-            <h4 className='h4 text-center my-5 font-semibold'>SCORE GAINS/LOSSES</h4>
-            <div className=''>
-              <BarGraph />
-            </div>
-          </div>
-          <div>
-            <h4 className='h4 text-center mt-60 mb-5 font-semibold'>WIN RATE</h4>
-            <PieGraph/>
-          </div> 
-          <div className='grid grid-cols-2 mt-60 py-10 scale-90'>
+      {hidden && (
+      <div aria-label='statistics & match-history' className=''>
+        <div aria-label='statistics' className='w-200 scale-90'>
+          <h3 className='h3 text-center font-semibold my-5 ml-15'>STATS</h3>
+          <div className='grid grid-cols-2 w-full h-200'>
 
-            <div className='flex flex-col items-center'>
-              <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
-                <div className='text-5xl font-bold text-black'>{playedGames}</div>
+            <div>
+              <h4 className='h4 text-center my-5 font-semibold'>SCORE HISTORY</h4>
+              <LineGraph />
+            </div>
+
+            <div>
+              <h4 className='h4 text-center my-5 font-semibold'>SCORE GAINS/LOSSES</h4>
+              <div className=''>
+                <BarGraph />
               </div>
-              <h4 className='h4 my-2 font-semibold'>PLAYED GAMES</h4>
             </div>
 
-            <div className='flex flex-col items-center'>
-              <button className='group relative flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center}'>
-                <div className='absolute text-3xl -top-12 left-1/2 -translate-x-1/2 text-black opacity-0 
-                                group-hover:opacity-100 transition-opacity ease-in-out'>{worstRival}</div>
-                {WorstRivalPic}
-              </button>
-              <h4 className='h4 my-2 font-semibold'>WORST RIVAL</h4>
-            </div>
+            <div>
+              <h4 className='h4 text-center mt-60 mb-5 font-semibold'>WIN RATE</h4>
+              <PieGraph/>
+            </div> 
 
-            <div className='flex flex-col items-center'>
-              <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
-                <div className='text-5xl font-bold text-black'>{winStreak}</div>
+            <div className='grid grid-cols-2 mt-60 py-10 scale-90'>
+
+              <div className='flex flex-col items-center'>
+                <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
+                  <div className='text-5xl font-bold text-black'>{playedGames}</div>
+                </div>
+                <h4 className='h4 my-2 font-semibold'>PLAYED GAMES</h4>
               </div>
-              <h4 className='h4 my-2 font-semibold'>WIN STREAK</h4>
-            </div>
 
-            <div className='flex flex-col items-center'>
-              <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
-                <div className='text-5xl font-bold text-black'>{longestWinStreak}</div>
+              <div className='flex flex-col items-center'>
+                <button className='group relative flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center}'>
+                  <div className='absolute text-3xl -top-12 left-1/2 -translate-x-1/2 text-black opacity-0 
+                                  group-hover:opacity-100 transition-opacity ease-in-out'>{worstRival}</div>
+                  {WorstRivalPic}
+                </button>
+                <h4 className='h4 my-2 font-semibold'>WORST RIVAL</h4>
               </div>
-              <h4 className='h4 my-2 font-semibold text-center'>LONGEST WIN STREAK</h4>
+
+              <div className='flex flex-col items-center'>
+                <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
+                  <div className='text-5xl font-bold text-black'>{winStreak}</div>
+                </div>
+                <h4 className='h4 my-2 font-semibold'>WIN STREAK</h4>
+              </div>
+
+              <div className='flex flex-col items-center'>
+                <div className='flex size-25 rounded-full border-4 border-black bg-[#FFCC00] items-center justify-center'>
+                  <div className='text-5xl font-bold text-black'>{longestWinStreak}</div>
+                </div>
+                <h4 className='h4 my-2 font-semibold text-center'>LONGEST WIN STREAK</h4>
+              </div>
+
             </div>
-          
           </div>
         </div>
-      </div>
 
-
-      {/* MATCH HISTORY */}
-      <div className=''>
-        <h3 className='h3 text-center font-semibold mb-5 -mt-10 scale-90'>MATCH HISTORY</h3>
-        <MatchHistory player1={user?.username} />
-      </div>
-    </div>
-  );
-};
+          <div aria-label='match history'>
+            <h3 className='h3 text-center font-semibold mb-5 -mt-10 scale-90'>MATCH HISTORY</h3>
+            <MatchHistory player1={user?.username} />
+          </div>
+          </div>)}
+        </div>)}
+    
 
 
 export default HomeUserPage;
 
 
-          // <div className='mt-15 ml-15 px-2 flex flex-col'>
+          {/* // <div className='mt-15 ml-15 px-2 flex flex-col'>
           //   <div className='flex mb-5 justify-between items-end border-b-2 border-dashed'>
           //     <h4 className='h4 font-semibold'>PLAYED GAMES</h4><h3 className='h3 font-bold'>{playedGames}</h3>
           //   </div>
@@ -166,4 +177,4 @@ export default HomeUserPage;
           //   </div>
           //   <div className='flex my-5 justify-between items-end border-b-2 border-dashed'>
           //     <h4 className='h4 font-semibold'>WORST RIVAL</h4><h3 className='h3 font-bold'>{worstRival}</h3>
-          //   </div>
+          //   </div> */}
