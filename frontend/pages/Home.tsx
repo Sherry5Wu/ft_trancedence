b72f262f6cf2441f13ca83579acdedf220e4ae57
Helena Utzig <helenautzig@gmail.com>
2025-08-11 14:19:54 +0300

    WIP Multiple language module

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -3,9 +3,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GenericButton } from '../components/GenericButton';
+import { useTranslation } from 'react-i18next';
+import DownArrow from '../assets/noun-down-arrow-down-1144832.svg?react';
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate(); // to access other pages
+  const { t, i18n } = useTranslation();
 
   return (
     <div className="flex flex-col items-center p-8 space-y-6">
@@ -13,13 +16,16 @@ const HomePage: React.FC = () => {
       {/* Page title*/}
       <div>
         <h1 className="h1 mb-30">P | N G - P · N G</h1>
+
+        <p>{t('welcome')}</p>
       </div>
 
       {/* Sign in Button */}
       <div className="flex flex-wrap justify-center align-middle gap-6">
         <GenericButton
           className="generic-button"
-          text="SIGN IN"
+          // add on component    ariaLabel={t('signIn')}  
+          text={t('signIn')}
           icon={undefined}
           hoverLabel={undefined}
           disabled={false}
