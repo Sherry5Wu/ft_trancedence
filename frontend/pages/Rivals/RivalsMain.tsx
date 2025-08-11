812193563347ea1482df50f07637d19592b966ec
Joel Lehtonen <jlehtone@c1r6p8.hive.fi>
2025-08-11 15:59:40 +0300

    fix: rival search bar looks right, no functionality yet

--- frontend/pages/Rivals/RivalsMain.tsx
+++ frontend/pages/Rivals/RivalsMain.tsx
@@ -1,14 +1,21 @@
 // pages/Rivals/RivalsMain.tsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RivalRows } from '../../components/RivalRows';
 import { GenericInput } from '../../components/GenericInput';
+import { useValidationField } from '../../hooks/useValidationField';
+import { isValidUsername } from '../../utils/Validation';
 import RivalIcon from '../../assets/noun-battle-7526810.svg?react';
 import SearchIcon from '../../assets/noun-search-7526678.svg?react';
 
 const RivalsPage = () => {
     const navigate = useNavigate();
+    const searchField = useValidationField('', isValidUsername);
+
+    useEffect(() => {
+
+    })
   
     return (
       <div className='pageLayout'>
@@ -19,15 +26,26 @@ const RivalsPage = () => {
           <RivalIcon className='size-20' />
         </div>
 
-        <div className='flex justify-left items-center'>
-          <SearchIcon className='menuIcon' />
-          <form className=''>
-            <GenericInput placeholder='Search for new rivals' value='' className='h-8 w-56 bg-[#FFEE8C] rounded-full mb-3 border-2 border-transparent 
-                              hover:border-black transition ease-in-out duration-300  hover:cursor-pointer'/>
-          </form>
-        </div>
+        <div className='min-w-100'>
+          <div aria-label='rival search bar' className='flex justify-start items-center group -translate-x-12'>
+            <SearchIcon className='size-12 translate-x-12 -translate-y-0.5 z-10 group-focus-within:opacity-50 transition ease-in-out duration-300'/>
+            <div className=''>
+                <GenericInput
+                  type="username"
+                  placeholder="Search for new rivals"
+                  value={searchField.value}
+                  onFilled={searchField.onFilled}
+                  onBlur={searchField.onBlur}
+                  // errorMessage={searchField.error}
+                  className='h-10 w-64 pl-12 bg-[#FFEE8C] rounded-full mb-3 border-2 border-transparent 
+                                hover:border-black transition-all ease-in-out duration-200 hover:cursor-pointer
+                                focus:border-[#4682B4]'
+          />
+            </div>
+          </div>
 
-        <RivalRows />
+          <RivalRows />
+        </div>
 
       </div>
     );
