1ae8732a2ba82ff73d3cb7e66595ace3cd83cdd0
Helena Utzig <helenautzig@gmail.com>
2025-08-14 10:47:18 +0300

    pages/SignIn translation and ARIA attributes && Google sign in button

--- frontend/components/CustomGoogleLoginButton.tsx
+++ frontend/components/CustomGoogleLoginButton.tsx
@@ -1,6 +1,9 @@
 import { useGoogleLogin } from '@react-oauth/google';
+import { useTranslation } from 'react-i18next';
 
 const CustomGoogleLoginButton = () => {
+  const { t } = useTranslation();  // Destructure `t` from useTranslation hook
+
   const login = useGoogleLogin({
     onSuccess: tokenResponse => {
       console.log("Login Success:", tokenResponse);
@@ -19,16 +22,17 @@ const CustomGoogleLoginButton = () => {
   >
     <svg
       viewBox="0 0 533.5 544.3"
-      // xmlns="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
+      aria-label={t('pages.signInWithGoogle.aria.signInWithGoogle')}
       focusable="false"
     >
-      <path fill="#4285f4" d="M533.5 278.4c0-17.4-1.4-34.3-4.1-50.6H272v95.7h146.9c-6.4 34.7-25.5 64.1-54.5 83.6v69.3h87.9c51.4-47.3 81.2-117 81.2-197.9z" />
-      <path fill="#34a853" d="M272 544.3c73.7 0 135.5-24.5 180.7-66.4l-87.9-69.3c-24.4 16.4-55.5 26-92.8 26-71.4 0-132-48.1-153.7-112.6H28.4v70.7c45.1 89 137.9 151.6 243.6 151.6z" />
-      <path fill="#fbbc05" d="M118.3 322c-10.3-30.5-10.3-63.7 0-94.2V157H28.4c-37.6 73.8-37.6 160.5 0 234.3l89.9-69.3z" />
-      <path fill="#ea4335" d="M272 107.7c39.9 0 75.8 13.7 104 40.8l78-78C407.5 25.6 345.7 0 272 0 166.3 0 73.5 62.6 28.4 151.7l89.9 69.3C140 155.8 200.6 107.7 272 107.7z" />
+        <path fill="#4285f4" d="M533.5 278.4c0-17.4-1.4-34.3-4.1-50.6H272v95.7h146.9c-6.4 34.7-25.5 64.1-54.5 83.6v69.3h87.9c51.4-47.3 81.2-117 81.2-197.9z" />
+        <path fill="#34a853" d="M272 544.3c73.7 0 135.5-24.5 180.7-66.4l-87.9-69.3c-24.4 16.4-55.5 26-92.8 26-71.4 0-132-48.1-153.7-112.6H28.4v70.7c45.1 89 137.9 151.6 243.6 151.6z" />
+        <path fill="#fbbc05" d="M118.3 322c-10.3-30.5-10.3-63.7 0-94.2V157H28.4c-37.6 73.8-37.6 160.5 0 234.3l89.9-69.3z" />
+        <path fill="#ea4335" d="M272 107.7c39.9 0 75.8 13.7 104 40.8l78-78C407.5 25.6 345.7 0 272 0 166.3 0 73.5 62.6 28.4 151.7l89.9 69.3C140 155.8 200.6 107.7 272 107.7z" />
     </svg>
-    Sign in with Google
+      {t('pages.signInWithGoogle.button')}
   </button>
   );
 };
--- frontend/pages/SignIn.tsx
+++ frontend/pages/SignIn.tsx
@@ -1,6 +1,10 @@
-// pages/SignIn.tsx
+// /src/pages/SignIn.tsx
 
 import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import CustomGoogleLoginButton from "../components/CustomGoogleLoginButton";
+import { AccessiblePageDescription } from '../components/AccessiblePageDescription';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useState } from "react";
@@ -9,7 +13,10 @@ import { GenericInput} from "../components/GenericInput";
 import { useValidationField } from '../hooks/useValidationField';
 import { isValidUsername, isValidEmail, isValidPassword } from '../utils/Validation';
 
+const clientId = "604876395020-v57ifnl042bi718lgm2lckhpbfqdog6b.apps.googleusercontent.com";
+
 const SignInPage: React.FC = () => {
+  const { t } = useTranslation();  
   const navigate = useNavigate();
 
   const usernameField = useValidationField('', isValidUsername);
@@ -19,45 +26,68 @@ const SignInPage: React.FC = () => {
     usernameField.value !== '' &&
     passwordField.value !== '';
 
-  return (
-    <div className="flex flex-col justify-center p-8 space-y-4 max-w-sm mx-auto">
-      
-      <h3 className="font-semibold text-center">Sing In</h3>
+return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <main
+        className="pageLayout"
+        role="main"
+        aria-labelledby="pageTitle"
+        aria-describedby="pageDescription"
+      >
+        <AccessiblePageDescription
+          id="pageDescription"
+          text={t('pages.signIn.aria.description')}
+        />
 
-      <GenericInput
-        type="username"
-        placeholder="Username"
-        value={usernameField.value}
-        onFilled={usernameField.onFilled}
-        onBlur={usernameField.onBlur}
-        errorMessage={usernameField.error}
-      />
-      <GenericInput
-        type="password"
-        placeholder="Password"
-        value={passwordField.value}
-        onFilled={passwordField.onFilled}
-        onBlur={passwordField.onBlur}
-        errorMessage={passwordField.error}
-      />
+        <h1 id="pageTitle" className="font-semibold text-center text-xl">
+          {t('pages.signIn.title')}
+        </h1>
 
-      <GenericButton
-        className="generic-button"
-        text="LOG IN"
-        disabled={!formFilled}
-        onClick={() => { navigate('/homeuser')}}
-      />
+        <GenericInput
+          type="text"
+          placeholder={t('common.placeholders.username')}
+          aria-label={t('common.aria.inputs.username')}
+          value={usernameField.value}
+          onFilled={usernameField.onFilled}
+          onBlur={usernameField.onBlur}
+          errorMessage={usernameField.error}
+        />
 
-      {/* GoogleLogin Button */}
+        <GenericInput
+          type="password"
+          placeholder={t('common.placeholders.password')}
+          aria-label={t('common.aria.inputs.password')}
+          value={passwordField.value}
+          onFilled={passwordField.onFilled}
+          onBlur={passwordField.onBlur}
+          errorMessage={passwordField.error}
+        />
 
-      {/* Sign up link */}
-      <p className="text-center text-sm">
-        Not registered?{' '}
-        <Link to="/signup" className="text: underline">
-          Sign up
-        </Link>
-      </p>
-    </div>
+        <GenericButton
+          className="generic-button"
+          text={t('common.buttons.logIn')}
+          aria-label={t('common.aria.buttons.logIn')}
+          disabled={!formFilled}
+          onClick={() => navigate('/homeuser')}
+        />
+
+        {/* Google Sign-In */}
+        <CustomGoogleLoginButton
+          aria-label={t('pages.signInWithGoogle.aria.signInWithGoogle')}
+        />
+
+        <p className="text-center text-sm">
+          {t('pages.signIn.links.notRegistered')}{' '}
+          <Link
+            to="/signup"
+            className="underline"
+            aria-label={t('pages.signIn.aria.signUpLink')}
+          >
+            {t('pages.signIn.links.signUp')}
+          </Link>
+        </p>
+      </main>
+    </GoogleOAuthProvider>
   );
 };
 
