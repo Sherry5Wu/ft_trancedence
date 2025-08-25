f11489a4e2c51a261ce12d1b523cd55390285e6f
Jingjing Wu <sherry.helenius@gmail.com>
2025-08-25 11:27:51 +0300

    added refreshToken cookies, and 2FA and registerFromGoole items into login apis

--- services/auth-service/src/utils/authCookie.js
+++ services/auth-service/src/utils/authCookie.js
@@ -1,6 +1,6 @@
-function setAuthCookie(reply, token, isAccessToken = true) {
+function setRefreshTokenCookie(reply, token) {
   // use __Host- prefix (host-only, most secure)
-  const name = isAccessToken ? '__Host-accessToken' : '__Host-refreshToken';
+  const name = '__Host-refreshToken';
   const COOKIE_SECURE = process.env.NODE_ENV === 'production';
 
   reply.setCookie(name, token, {
@@ -8,14 +8,14 @@ function setAuthCookie(reply, token, isAccessToken = true) {
     secure: COOKIE_SECURE, // ensures HTTPS, environment-aware mode
     sameSite: 'Lax', // prevents CSRF, Lax is common compromise
     path: '/',
-    maxAge: isAccessToken ? 60 * 15 : 60 * 60 * 24 * 7 // 15 mins(accessToken) or 7 days (refreshToken)
+    maxAge: 60 * 60 * 24 * 7 // 7 days
   });
 }
 
-function clearAuthCookie(reply, isAccessToken = true) {
-  const name = isAccessToken ? '__Host-accessToken' : '__Host-refreshToken';
+function clearRefreshTokenCookie(reply) {
+  const name = '__Host-refreshToken';
 
   reply.clearCookie(name, { path: '/' });
 }
 
-export { setAuthCookie, clearAuthCookie };
+export { setRefreshTokenCookie, clearRefreshTokenCookie };
